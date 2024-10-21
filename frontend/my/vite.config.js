e85ffc107fd30f8bfc112da80c1467aaa1b5b786
Practice <coding4443@gmail.com>
2024-10-21 11:37:14 -0700

    new changes

--- frontend/my/vite.config.js
+++ frontend/my/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  build: {
-    outDir: 'dist', // Ensure this matches your Vercel output directory setting
-  },
-
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This will be the output directory for the production build
+    rollupOptions: {
+      // The 'external' option is not needed unless you want to externalize certain modules
+    },
+  },
+});

5236290e91ea34039b0e08630b6cf7b80706375c
Practice <coding4443@gmail.com>
2024-10-25 11:41:35 -0700

    change dist

--- frontend/my/vite.config.js
+++ frontend/my/vite.config.js
@@ -1,13 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'frontend/my/dist',   // This will be the output directory for the production build
-    rollupOptions: {
-      // The 'external' option is not needed unless you want to externalize certain modules
-    },
-  },
+    outDir: 'dist' // Ensure this is set to 'dist'
+  }
 });
